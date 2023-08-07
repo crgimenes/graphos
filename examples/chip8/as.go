a7f468d9055c949aa504a2e82a0cc74eae8c35b3
crgimenes <crg@crg.eti.br>
2023-08-06 22:14:36 -0300

    add assembly and disassembly draft functions

---
+++ examples/chip8/as.go
@@ -0,0 +1,11 @@
+package main
+
+// Assemble chip8 binary
+func Assemble(asm []string) ([]uint16, error) {
+	return nil, nil
+}
+
+// Disassemble chip8 binary
+func Disassemble(rom []uint16) ([]string, error) {
+	return nil, nil
+}
